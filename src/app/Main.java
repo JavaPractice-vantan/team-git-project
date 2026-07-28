b26d3dac5f49ae2535bfba002e418f0f805f2d9e
Yuta Otake <otakeyuta1009@gmail.com>
2026-07-28 17:42:34 +0900

    Mainクラスの入力処理を追加

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,26 +3,34 @@ package app;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("-----メニュー一覧-----");
+
+		System.out.println("=====四則演算プログラム=====");
+		System.out.println("=======メニュー一覧=======");
 		System.out.println("1: 足し算プログラム");
 		System.out.println("2: 引き算プログラム");
 		System.out.println("3: 掛け算プログラム");
 		System.out.println("4: 割り算プログラム");
-		System.out.print("入力:");
+		System.out.print("実行する演算を選択してください: ");
 
-		int action = scanner.nextInt();
+		int menuNumber = scanner.nextInt();
 
-		if (action == 1) {
+		if (menuNumber == 1) {
 
-		} else if (action == 2) {
+		} else if (menuNumber == 2) {
 
-		} else if (action == 3) {
+		} else if (menuNumber == 3) {
 
-		} else if (action == 4) {
+		} else if (menuNumber == 4) {
 
+		} else {
+			System.out.println("1から4の整数で入力してください。");
 		}
+
+		scanner.close();
 
 	}
 }
