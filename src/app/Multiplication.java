973709c917e6b3f1629f0033a7ee9e7f7d36e65a
kubotaiji <kubotaiji@10.95.1.241>
2026-07-29 13:39:06 +0900

    Multiplicationのコードです。

--- src/app/Multiplication.java
+++ src/app/Multiplication.java
@@ -1,5 +1,11 @@
 package app;
 
 public class Multiplication {
+	public static void main(String[] args) {
+		int num1 = 8;
+		int num2 = 9;
+		int result = num1 * num2;
 
+		System.out.println(num1 + " × " + num2 + " = " + result);
+	}
 }
